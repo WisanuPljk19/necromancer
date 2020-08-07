3b10136e5b8fab28df876b3ae43ab98371f7b97b
Wisanu.pljk <wisanu.pljk@gmail.com>
2020-08-07 13:40:04 +0700

    change code to public

--- Sources/Necromancer/Necromancer.swift
+++ Sources/Necromancer/Necromancer.swift
@@ -1,3 +1,6 @@
-public struct Necromancer {
-    var text = "Hello, World!"
+public class Necromancer {
+    public var text = "Hello, World!"
+    public class func printAnyWord(){
+        print("call printAnyWord")
+    }
 }
